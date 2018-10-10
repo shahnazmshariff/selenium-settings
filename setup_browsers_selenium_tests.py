4bfec1c3bd7754ad7be1374744faf6446fa8c5b5
shahnazmshariff <shahnazmshariff@gmail.com>
2018-10-10 17:16:47 -0400

    add note in script

--- setup_browsers_selenium_tests.py
+++ setup_browsers_selenium_tests.py
@@ -6,6 +6,7 @@ def setup_headless_chrome():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
+    #optional - to set the size of disk cache as 0
     chrome_options.add_argument('--disk-cache-size=0')
     chrome_options.add_argument('--window-size=1200,1100')
     path_to_chromedriver = '/home/shahnaz/Downloads/chromedriver'
@@ -16,7 +17,10 @@ def setup_headless_chrome():
         'getLog': ('POST', '/session/$sessionId/log')})
 
 
+#Note: if tests are running in parallel, we need to synchornize webdriver creation
+# acquire and release lock before and after this function is called to solve the above problem
 def setup_chrome_with_xvfb():
+
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
 
