20f44e47a8f14b993c7d7ce450426357a73d9fdb
shahnazmshariff <shahnazmshariff@gmail.com>
2018-10-10 17:26:54 -0400

    remove commands to create log files

--- setup_browsers_selenium_tests.py
+++ setup_browsers_selenium_tests.py
@@ -10,32 +10,18 @@ def setup_headless_chrome():
     chrome_options.add_argument('--disk-cache-size=0')
     chrome_options.add_argument('--window-size=1200,1100')
     path_to_chromedriver = '/home/shahnaz/Downloads/chromedriver'
-    driver = webdriver.Chrome(path_to_chromedriver, chrome_options=chrome_options,
-                                   desired_capabilities={'loggingPrefs': {'performance': 'INFO', 'driver': 'INFO'}})
-    driver.command_executor._commands.update({
-        'getAvailableLogTypes': ('GET', '/session/$sessionId/log/types'),
-        'getLog': ('POST', '/session/$sessionId/log')})
+    driver = webdriver.Chrome(path_to_chromedriver, chrome_options=chrome_options)
 
 
 #Note: if tests are running in parallel, we need to synchornize webdriver creation
 # acquire and release lock before and after this function is called to solve the above problem
 def setup_chrome_with_xvfb():
-
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
-
     display = Display(visible=0, size=(1200, 1100))
-
     display.start()
-
     path_to_chromedriver = '/home/shahnaz/Downloads/chromedriver'
-
-    driver = webdriver.Chrome(path_to_chromedriver, chrome_options=chrome_options,
-                                   desired_capabilities={'loggingPrefs': {'performance': 'INFO', 'driver': 'INFO'}})
-    driver.command_executor._commands.update({
-        'getAvailableLogTypes': ('GET', '/session/$sessionId/lo'
-                                        'g/types'),
-        'getLog': ('POST', '/session/$sessionId/log')})
+    driver = webdriver.Chrome(path_to_chromedriver, chrome_options=chrome_options)
 
 
 
